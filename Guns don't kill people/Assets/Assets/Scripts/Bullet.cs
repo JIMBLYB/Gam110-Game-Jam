f8db5fe2a68bd30f3eacafe9c9bc97b274ad12b2
James Berry <git@jamesberry.co.uk>
2019-10-18 15:23:46 +0100

    Added physics material for bullet and bullet aiming script (both unfinished)

---
+++ Guns don't kill people/Assets/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction;
+    private Rigidbody2D rb2d;
+
+    private void Update()
+    {      
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            direction = Input.mousePosition - transform.position;
+            direction *= speed;
+
+            rb2d.AddForce(direction);
+        }
+    } 
+}
