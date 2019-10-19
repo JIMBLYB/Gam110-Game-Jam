72d5a1bc59659a675a1defcdc672a4bb9825a003
Christopher Robertson <christopherprobertson@gmail.com>
2019-10-19 18:40:17 +0100

    Added the PlayerAim.cs class which deals with the aiming of the player. The code itself coherently works, but I was experimenting with the offsets of the player and camera in the mouse position, so it is currently offset incorrectly, but will be fixed tomorrow. I also made some attempts at locking the rotation, but they haven't been successful yet due to issues with lerping and limiting a rotation.

---
+++ Guns don't kill people/Assets/Scripts/Gameplay/PlayerAim.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform headPivot;
+    [SerializeField]
+    private float aimRotationSpeed = 1.25f;
+
+    private Vector3 mousePosition;
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    { 
+        mousePosition = GetMousePos(Input.mousePosition);
+        AimingGun(mousePosition.normalized);
+        FlipSprite();
+        //LimitRotation();
+    }
+
+    private void FlipSprite()
+    {
+        //print(mousePosition.x - mainCamera.transform.position.x);
+        if (mousePosition.x - headPivot.position.x >= 0)
+        {
+            print("Looking Right");
+            return;
+        }
+
+        else
+        {
+            print("Looking Left");
+        }
+    }
+
+    private void AimingGun(Vector3 direction)
+    {
+        //print(headPivot.transform.up - headPivot.transform.right);
+        //print(headPivot.transform.right - headPivot.transform.up);
+        //print(headPivot.transform.right + headPivot.transform.up);
+
+        direction -= headPivot.position;
+        LerpDirection(direction);
+    }
+
+    private void LerpDirection(Vector3 direction)
+    {
+        Vector3 lerpPosition = Vector3.Lerp(headPivot.transform.right, direction, Time.deltaTime * aimRotationSpeed);
+        headPivot.transform.right = lerpPosition;
+    }
+
+    private void LerpRotation(Vector3 rotation)
+    {
+        Vector3 lerpPosition = Vector3.Lerp(headPivot.transform.eulerAngles, rotation, Time.deltaTime * aimRotationSpeed);
+        headPivot.transform.rotation = Quaternion.Euler(lerpPosition);
+    }
+
+    private void LimitRotation()
+    {
+        float headPivotRotationZ = headPivot.transform.eulerAngles.z;
+        headPivotRotationZ = Mathf.Clamp(headPivotRotationZ, -90, 180);
+
+        headPivot.transform.rotation = Quaternion.Euler(new Vector3(headPivot.transform.eulerAngles.x,
+                                                                    headPivot.transform.eulerAngles.y,
+                                                                    headPivotRotationZ));
+
+    }
+
+    private Vector3 GetMousePos(Vector3 screenMousePos)
+    {
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(screenMousePos);
+        return new Vector3(mousePos.x - mainCamera.transform.position.x, mousePos.y - mainCamera.transform.position.y, 0);
+    }
+}
