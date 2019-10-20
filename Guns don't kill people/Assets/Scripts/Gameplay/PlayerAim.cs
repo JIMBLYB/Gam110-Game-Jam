e3ed0ba604e0ea4663d4354f1ef3dd35996614fa
Christopher Robertson <christopherprobertson@gmail.com>
2019-10-20 11:54:21 +0100

    Altered the AimingGun to firstly now just directly aim at an object rather than lerp towards it since it provided too many unwanted effects when it came to the flipping of the sprite. The aiming rotation lock was also altered to use the mouse position on the y axis to determine when to lock it rather than just rotation which was quite inconsistent and harder to lock.

--- Guns don't kill people/Assets/Scripts/Gameplay/PlayerAim.cs
+++ Guns don't kill people/Assets/Scripts/Gameplay/PlayerAim.cs
@@ -3,9 +3,16 @@
 public class PlayerAim : MonoBehaviour
 {
     [SerializeField]
+    private Transform playerParent;
+    [SerializeField]
     private Transform headPivot;
     [SerializeField]
     private float aimRotationSpeed = 1.25f;
+    [Range(-1, 1)]
+    [SerializeField]
+    private float rotationLimiter;
+
+    private Vector3 originalHeadScale;
 
     private Vector3 mousePosition;
     private Camera mainCamera;
@@ -13,67 +20,44 @@ public class PlayerAim : MonoBehaviour
     private void Start()
     {
         mainCamera = Camera.main;
+        originalHeadScale = playerParent.localScale;
     }
 
     private void Update()
     { 
         mousePosition = GetMousePos(Input.mousePosition);
-        AimingGun(mousePosition.normalized);
-        FlipSprite();
-        //LimitRotation();
+
+        print(mousePosition.normalized.y);
+        if (mousePosition.normalized.y > rotationLimiter)
+        {
+            FlipSprite();
+        }
     }
 
     private void FlipSprite()
     {
-        //print(mousePosition.x - mainCamera.transform.position.x);
-        if (mousePosition.x - headPivot.position.x >= 0)
+        if (mousePosition.x >= 0)
         {
-            print("Looking Right");
+            playerParent.localScale = originalHeadScale;
+            AimingGun(mousePosition.normalized);
             return;
         }
 
         else
         {
-            print("Looking Left");
+            playerParent.localScale = new Vector3(-originalHeadScale.x, originalHeadScale.y, originalHeadScale.z);
+            AimingGun(-mousePosition.normalized);
         }
     }
 
     private void AimingGun(Vector3 direction)
     {
-        //print(headPivot.transform.up - headPivot.transform.right);
-        //print(headPivot.transform.right - headPivot.transform.up);
-        //print(headPivot.transform.right + headPivot.transform.up);
-
-        direction -= headPivot.position;
-        LerpDirection(direction);
-    }
-
-    private void LerpDirection(Vector3 direction)
-    {
-        Vector3 lerpPosition = Vector3.Lerp(headPivot.transform.right, direction, Time.deltaTime * aimRotationSpeed);
-        headPivot.transform.right = lerpPosition;
-    }
-
-    private void LerpRotation(Vector3 rotation)
-    {
-        Vector3 lerpPosition = Vector3.Lerp(headPivot.transform.eulerAngles, rotation, Time.deltaTime * aimRotationSpeed);
-        headPivot.transform.rotation = Quaternion.Euler(lerpPosition);
-    }
-
-    private void LimitRotation()
-    {
-        float headPivotRotationZ = headPivot.transform.eulerAngles.z;
-        headPivotRotationZ = Mathf.Clamp(headPivotRotationZ, -90, 180);
-
-        headPivot.transform.rotation = Quaternion.Euler(new Vector3(headPivot.transform.eulerAngles.x,
-                                                                    headPivot.transform.eulerAngles.y,
-                                                                    headPivotRotationZ));
-
+        headPivot.transform.right = direction;
     }
 
     private Vector3 GetMousePos(Vector3 screenMousePos)
     {
         Vector3 mousePos = mainCamera.ScreenToWorldPoint(screenMousePos);
-        return new Vector3(mousePos.x - mainCamera.transform.position.x, mousePos.y - mainCamera.transform.position.y, 0);
+        return new Vector3(mousePos.x - headPivot.position.x, mousePos.y - headPivot.position.y, 0);
     }
 }
