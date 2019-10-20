92191bcdbd43e3a1e7981ce774c0102d7e9303f7
JamesB <git@jamesberry.co.uk>
2019-10-20 12:16:35 +0100

    Setup for breakable objects testing

---
+++ Guns don't kill people/Assets/Scripts/TestScripts/Breakable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Breaking : MonoBehaviour
+{
+    public bool object_Bounces;
+    [SerializeField]
+    private Collider2D collider;
+
+    private void Start()
+    {
+        if (object_Bounces)
+        {
+            collider.isTrigger = false;
+        }
+        else if (!object_Bounces)
+        {
+            collider.isTrigger = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D colision)
+    {
+
+    }
+}
