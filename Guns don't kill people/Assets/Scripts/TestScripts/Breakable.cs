5b294973359e5f9b57ecde49a0de326b9df246d0
JamesB <git@jamesberry.co.uk>
2019-10-22 18:28:13 +0100

    Added GravityAdjust.cs which allows you to change the scale of gravity on collision.

--- Guns don't kill people/Assets/Scripts/TestScripts/Breakable.cs
+++ Guns don't kill people/Assets/Scripts/TestScripts/Breakable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Breaking : MonoBehaviour
+public class Breakable : MonoBehaviour
 {
     public bool object_Bounces;
     [SerializeField]
