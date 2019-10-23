f19364ab0c7dbe4cc463abb3cf0eae392d84ebde
Kyle James <kyleajames102@gmail.com>
2019-10-23 20:48:00 +0100

    Added bullet hit scripts and bullet sprite

---
+++ Guns don't kill people/Assets/Scripts/Gameplay/BulletHits.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHits : MonoBehaviour
+{
+    private int hitsTaken;
+
+    [SerializeField]
+    private int hitsNeeded = 3;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        hitsTaken += 1;
+        if (hitsTaken >= hitsNeeded)
+            Destroy(gameObject);
+    }
+}
