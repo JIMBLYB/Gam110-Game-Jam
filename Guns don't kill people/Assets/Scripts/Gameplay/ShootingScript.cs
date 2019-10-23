b671e5de47e089967edb34e6004a9e3f7d68d903
Edward Peters <EP231933@falmouth.ac.uk>
2019-10-23 20:44:49 +0100

    Added shot limit to Shooting script

---
+++ Guns don't kill people/Assets/Scripts/Gameplay/ShootingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform barrelTip;
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    private int shots = 3;
+    
+    void Update()
+    {
+        
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (shots > 0)
+            {
+                FireBullet();
+                shots = shots - 1;
+            }
+        }
+    }
+    private void FireBullet()
+    {
+        GameObject firedBullet = Instantiate(bullet, barrelTip.position, barrelTip.rotation);
+        firedBullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.right * speed );
+    }
+}
