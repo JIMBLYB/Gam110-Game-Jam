308b4f36c89430b6d3077167d900a8559c054cf9
Christopher Robertson <christopherprobertson@gmail.com>
2019-10-23 20:37:12 +0100

    Added the ChangeScene.cs script to allow the restarting of the level with a specific key provided - currently 'R' - as well as a function that can be called at any time. It is also a singleton as well, so only one can and will need to be active at one time. I also added the ObjectDropper.cs which deals with dropping the ball on the player as well as the respective animation and particle effects which has been added and implemented also.

---
+++ Guns don't kill people/Assets/Scripts/Gameplay/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public static ChangeScene Instance;
+    private void Awake()
+    {
+        if (Instance != null) Instance = this;
+        else Destroy(this);
+    }
+
+    private void Update()
+    {
+        RestartScene(KeyCode.R);
+    }
+
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    private void RestartScene(KeyCode restartButton)
+    {
+        if (Input.GetKeyDown(restartButton))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
---
+++ Guns don't kill people/Assets/Scripts/Gameplay/ObjectDropper.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public enum Tags
+{
+    Bullet = 0
+};
+
+
+[RequireComponent(typeof(Animator))]
+public class ObjectDropper : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToDrop;
+    private Rigidbody2D objectToDropRigidBody;
+    private bool hasDropped = false;
+
+    [SerializeField]
+    private string wireAnimTriggerName = "Dropped";
+    private Animator wireAnim;
+
+    [SerializeField]
+    private GameObject brokenWireParticle;
+
+    private void Start()
+    {
+        hasDropped = false;
+
+        objectToDropRigidBody = objectToDrop.GetComponent<Rigidbody2D>();
+        wireAnim = this.GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(Tags.Bullet.ToString()))
+        {
+            Destroy(collision.gameObject);
+            DropObject();
+        }
+    }
+
+    private void DropObject()
+    {
+        wireAnim.SetTrigger(wireAnimTriggerName);
+        ParticleManager.Instance.SpawnParticle(brokenWireParticle, this.transform.position, Quaternion.identity);
+
+        hasDropped = true;
+        objectToDropRigidBody.isKinematic = false;
+    }
+}
